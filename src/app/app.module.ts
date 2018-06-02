fe46f5e6d6a5dcdbcc99904ac250ca7efb3c094b
Vinicius Mijoler <vinicius_mijoler@hotmail.com>
2018-06-02 16:56:32 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,69 @@
+import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import {AngularFireModule, FirebaseAppConfig} from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AuthProvider } from '../providers/auth/auth';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { SigninPage } from '../pages/signin/signin';
+import { SignupPage } from '../pages/signup/signup';
+import { UserProvider } from '../providers/user/user';
+import { CustomLoggedHeaderComponent } from '../components/custom-logged-header/custom-logged-header';
+import { CapitalizePipe } from '../pipes/capitalize/capitalize';
+import { ChatPage } from '../pages/chat/chat';
+import { ChatProvider } from '../providers/chat/chat';
+
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyBGLJ3ORodAvuGaDGDYZ9m_2j5HZIIxQ8w",
+  authDomain: "vm-ionic-first-app.firebaseapp.com",
+  databaseURL: "https://vm-ionic-first-app.firebaseio.com",
+  projectId: "vm-ionic-first-app",
+  storageBucket: "vm-ionic-first-app.appspot.com",
+  messagingSenderId: "1072774317202"
+}
+
+@NgModule({
+  declarations: [
+    ChatPage,
+    CapitalizePipe,
+    CustomLoggedHeaderComponent,
+    MyApp,
+    HomePage,
+    SignupPage,
+    SigninPage,
+  ],
+  imports: [
+    AngularFireModule.initializeApp(firebaseAppConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+   ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    ChatPage,
+    MyApp,
+    HomePage,
+    SignupPage,
+    SigninPage,
+  ],
+  providers: [
+    AuthProvider,
+    ChatProvider,
+    StatusBar,
+    SplashScreen,
+    UserProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
